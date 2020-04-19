9c626824fbb1b3612943bfeaf1fff3bec29df99d
Aidan Bjelke <aidanbjelke@gmail.com>
2020-04-19 03:14:50 -0600

    Added logic and timing to have control over input

--- Flappy Yoyo/Assets/Scripts/yoyo.cs
+++ Flappy Yoyo/Assets/Scripts/yoyo.cs
@@ -5,28 +5,46 @@ using UnityEngine;
 public class yoyo : MonoBehaviour
 {
     public float gravity = 2.0f;
+    public float tug = 4.0f;
+    public float tugRate = 6.0f;
+    float nextTugTime = 0f;
     private Vector3 gChange;
     private Vector3 tChange;
-    public GameObject stringObj;
+    bool tugging = false;
 
+    public GameObject stringObj;
     // Start is called before the first frame update
     void Start()
     {
         gChange = new Vector3(0f, -gravity*Time.deltaTime, 0f);
-        tChange = new Vector3(0f, 4.0f*Time.deltaTime, 0f);
+        tChange = new Vector3(0f, tug*Time.deltaTime, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         // GRAVITY
-        transform.position = transform.position + gChange;
-        stringObj.transform.position = stringObj.transform.position + gChange;
+        // transform.position = transform.position + gChange;
+        // stringObj.transform.position = stringObj.transform.position + gChange;
 
-        // TUG
-        if(Input.GetMouseButton(0)) {
-            transform.position = transform.position + tChange;
-            stringObj.transform.position = stringObj.transform.position + tChange;
+        Vector3 currChange;
+
+        if(Time.time > nextTugTime) {
+            tugging = false;
+            if(Input.GetMouseButton(0)) {
+                tugging = true;
+                nextTugTime = Time.time + 1f / tugRate;
+            }
         }
+
+        if(tugging) {   // accel up from tug
+            currChange = tChange + gChange;
+            transform.position = transform.position + currChange;
+            stringObj.transform.position = stringObj.transform.position + currChange;
+        } else { // otherwise just apply gravity
+            transform.position = transform.position + gChange;
+            stringObj.transform.position = stringObj.transform.position + gChange;
+        }
+        
     }
 }

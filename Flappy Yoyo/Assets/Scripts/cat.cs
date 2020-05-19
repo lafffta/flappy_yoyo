f7897285e01fb6e4576db8049972202019727954
Aidan Bjelke <aidanbjelke@gmail.com>
2020-05-19 01:01:13 -0600

    Got cat moving, adding colliders and rigidbody on player which is required for triggers on the cats to work

---
+++ Flappy Yoyo/Assets/Scripts/cat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    public float catSpeed = 1.0f;
+    private Vector3 catChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        catChange = new Vector3(-catSpeed*Time.deltaTime, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += catChange;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag.Equals("Player")) {
+            Debug.Log("Player hit cat!\n");
+        }
+    }
+}

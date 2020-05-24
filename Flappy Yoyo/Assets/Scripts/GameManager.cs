d8c26182e16f9f501a2bd1dd4a0e3a6a7a74716c
Aidan Bjelke <aidanbjelke@gmail.com>
2020-05-23 22:28:13 -0600

    Added gamemanager which spawns cats and added manual garbage collection so cats are destroyed when far behind the player

---
+++ Flappy Yoyo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float spawnTime = 2f;
+    public cat catPrefab;
+    private float currentGameTime = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        catPrefab.catChange = new Vector3(-catPrefab.catSpeed*Time.deltaTime, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentGameTime += Time.deltaTime;
+        if(currentGameTime >= spawnTime) {
+            currentGameTime = 0.0f;
+            Instantiate(catPrefab, new Vector3(12.5f, Random.Range(-2.26f, -6.26f), 0f), new Quaternion(0f,0f,-0.7071068f,0.7071068f));
+            // Instantiate(catPrefab, new Vector3(10.09f, -2.26f, 0f), new Quaternion(0f,0f,-0.7071068f,0.7071068f));
+            // Instantiate(catPrefab, new Vector3(5.09f, -6.26f, 0f), new Quaternion(0f,0f,-0.7071068f,0.7071068f));
+        }
+    }
+}
--- Flappy Yoyo/Assets/Scripts/cat.cs
+++ Flappy Yoyo/Assets/Scripts/cat.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class cat : MonoBehaviour
 {
     public float catSpeed = 1.0f;
-    private Vector3 catChange;
+    public Vector3 catChange;
     // Start is called before the first frame update
     void Start()
     {
-        catChange = new Vector3(-catSpeed*Time.deltaTime, 0f, 0f);
+        // catChange = new Vector3(-catSpeed*Time.deltaTime, 0f, 0f); // now handled in game manager so it is constant for all cats
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += catChange;
+        if(transform.position.x < -30f) {
+            Destroy(gameObject);
+        }
     }
 
     // private void OnTriggerEnter2D(Collider2D other) {
